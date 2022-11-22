531028dc06c4671ffbc3f25df73c2d6d5a613ac1
Santanu Agarwal <santanu.agarwal@humanfractal.io>
2022-11-22 15:17:26 +0530

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Santanu Agarwal on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
